cff61565586d6a9e7b0a4b1410f5d3b02bdeabe3
Rodrigo Lopes <rodrigolmti@gmail.com>
2019-02-16 14:49:32 -0200

    Add navigation backButton

--- app/src/main/java/com/arctouch/codechallenge/ui/MainActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/ui/MainActivity.kt
@@ -1,16 +1,49 @@
 package com.arctouch.codechallenge.ui
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.arctouch.codechallenge.R
-
-import kotlinx.android.synthetic.main.activity_main.*
+import com.arctouch.codechallenge.util.HOME_FRAGMENT_LABEL
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.label) {
+                HOME_FRAGMENT_LABEL -> {
+                    controlBackButtonVisibility(false)
+                }
+                else -> {
+                    controlBackButtonVisibility(true)
+                }
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                navController.popBackStack()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun controlBackButtonVisibility(visible: Boolean) {
+        supportActionBar?.let {
+            it.apply {
+                setDisplayHomeAsUpEnabled(visible)
+                setHomeButtonEnabled(visible)
+            }
+        }
     }
 }
--- app/src/main/java/com/arctouch/codechallenge/util/Constants.kt
+++ app/src/main/java/com/arctouch/codechallenge/util/Constants.kt
@@ -1,10 +1,15 @@
 package com.arctouch.codechallenge.util
 
-import java.util.*
+//# URLS
 
 const val BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
 const val POSTER_URL = "https://image.tmdb.org/t/p/w154"
-const val BASE_KEY = "1f54bd990f1cdfb230adb312546d765d"
 const val SERVER_URL = "http://api.themoviedb.org/3/"
+
+//# KEYS
+
+const val BASE_KEY = "1f54bd990f1cdfb230adb312546d765d"
+
+const val HOME_FRAGMENT_LABEL = "fragment_home"
 const val TIMEOUT = 120L
 
